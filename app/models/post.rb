efcc83f4a8a4cea81559ce4c54097c1a5ec0fd08
Julie Meng <julieannmeng@gmail.com>
2021-10-13 21:01:41 -0400

    commit tree af6a53e0893411766b93ea98d77597b7f16bf3fe

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        titleArr = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        found = false
+        
+        titleArr.map do |p|
+            if self.title.to_s.include? p
+                found = true
+            end
+        end
+
+        if not found
+            errors.add(:title, "Title is not sufficiently clickbaity, please try another title")
+        end
+    end
 end
+
+
+# titleArr = ["Won't Belive", "Secret", "Top <number?", "Guess", ]
+# if titleArr.include? :title
+#     errors.add(:title, "Title is not sufficiently clickbaity, please try another title")
+# end
